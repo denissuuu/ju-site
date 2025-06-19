02df7e1e349d3fb6ab89f083138fb2a8b87ad153
Denis <denis.onet@ynov.com>
2025-06-19 10:08:37 +0200

    fixed window

--- tshirt-store/assets/js/menu.js
+++ tshirt-store/assets/js/menu.js
@@ -1,9 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     const menuIcon = document.getElementById("menu-icon");
     const menuPanel = document.getElementById("menu-panel");
+    const closeMenu = document.getElementById("close-menu");
 
     // Toggle the menu visibility when the menu button is clicked
     menuIcon.addEventListener("click", () => {
         menuPanel.classList.toggle("active"); // Toggle the active class
+    });
+
+    // Close the menu when the close button is clicked
+    closeMenu.addEventListener("click", () => {
+        menuPanel.classList.remove("active");
     });
 });
